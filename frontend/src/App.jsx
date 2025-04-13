179d06548557ead85cc228c852127da077cf9963
Joanna <joannakhek@gmail.com>
2025-04-13 08:48:58 +0800

    feat: set up navigation bar and top bar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {Routes, Route} from 'react-router'
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar
+      />
+    </>
+  )
+}
+
+export default App
