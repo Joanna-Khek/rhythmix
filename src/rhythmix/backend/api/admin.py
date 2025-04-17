405e5a3639063d148a9e7e7d5585e66c8558ff14
Joanna <joannakhek@gmail.com>
2025-04-17 15:12:11 +0800

    feat: added prompt prediction, attributes, predictions and saved songs models to the admin site

--- src/rhythmix/backend/api/admin.py
+++ src/rhythmix/backend/api/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import APICheck, InitialPrediction
+from .models import (
+    APICheck,
+    PromptPrediction,
+    AttributeValues,
+    SongPrediction,
+    SavedSongs,
+)
 
 # Register your models here.
 admin.site.register(APICheck)
-admin.site.register(InitialPrediction)
+admin.site.register(PromptPrediction)
+admin.site.register(AttributeValues)
+admin.site.register(SongPrediction)
+admin.site.register(SavedSongs)
